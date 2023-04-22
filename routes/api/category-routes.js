f3f4ee5f4bbbe1cc322aac86a1918a37419ad09b
mkaye <mattkaye97@gmail.com>
2023-04-22 18:12:54 -0400

    Added delete route to delete a single category.

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -53,7 +53,7 @@ router.put('/:id', async (req, res) => {
       },
     });
     if (!updateCategory) {
-      res.status(404).json({ message: 'No Category with this id!' });
+      res.status(404).json({ message: 'No category with this id!' });
       return;
     }
     res.status(200).json(updateCategory);
@@ -62,8 +62,24 @@ router.put('/:id', async (req, res) => {
   }
 });
 
-router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
+// delete a category by its `id` value
+router.delete('/:id', async (req, res) => {
+  try {
+    const deleteCategory = await Category.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+
+    if (!deleteCategory) {
+      res.status(404).json({ message: 'No category found with this id!'});
+      return;
+    }
+
+    res.status(200).json(deleteCategory);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
